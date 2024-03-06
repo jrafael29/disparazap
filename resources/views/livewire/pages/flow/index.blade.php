2ca92fdd6e23dae1961f06a77d1dd41cd230e218
José Rafael <rafaelsilva9240@hotmail.com>
2024-03-06 11:29:48 -0300

    add header to message flow index page

--- resources/views/livewire/pages/flow/index.blade.php
+++ resources/views/livewire/pages/flow/index.blade.php
@@ -1,3 +1,10 @@
 <div>
+    <!-- HEADER -->
+    <x-header title="Fluxo de mensagens" separator progress-indicator>
+    </x-header>
+
+
+
     {{-- Care about people's approval and you will be their prisoner. --}}
+    <h1>página de fluxos linda</h1>
 </div>
