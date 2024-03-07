1770a5d2759dcd6e20f5366ee8664187db1d3a28
José Rafael <rafaelsilva9240@hotmail.com>
2024-03-06 23:01:34 -0300

    add header to instance index page

--- resources/views/livewire/pages/instance/index.blade.php
+++ resources/views/livewire/pages/instance/index.blade.php
@@ -1,4 +1,6 @@
 <div>
+    <!-- HEADER -->
+    <x-header title="Instancias" separator progress-indicator></x-header>
     {{-- Success is as dangerous as failure. --}}
     <livewire:instance.form />
 </div>
