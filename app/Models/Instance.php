c9365462bc69a0694ee818541f0451704c2db99f
José Rafael <rafaelsilva9240@hotmail.com>
2024-03-06 23:10:34 -0300

    add fillable field to instance model

--- app/Models/Instance.php
+++ app/Models/Instance.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Instance extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'phonenumber',
+        'online',
+        'token',
+        'active',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
