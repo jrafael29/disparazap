cfa09bb3f7f95cabe92aaea06ac50a029c9ce0c6
José Rafael <rafaelsilva9240@hotmail.com>
2024-03-06 22:02:02 -0300

    add footer on login page to redirect to login

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -13,6 +13,8 @@
                     <x-button label="Login" class="btn-primary" type="submit" spinner="save" />
                 </x-slot:actions>
             </x-form>
+            <p class="text-1xl">Não tem uma conta? <a class="text-blue-900"
+                    href="{{route('register')}}">Registre-se</a>.</p>
         </div>
         {{-- If your happiness depends on money, you will never be happy with yourself. --}}
     </div>
