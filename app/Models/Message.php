42ea636a4816f662e051e725f6f9d09f161b191f
José Rafael <rafaelsilva9240@hotmail.com>
2024-03-08 01:04:20 -0300

    create message model and migration

---
+++ app/Models/Message.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'type_id', 'text', 'filepath', 'position', 'delay'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function type()
+    {
+        return $this->belongsTo(MessageType::class, 'type_id', 'id');
+    }
+}
