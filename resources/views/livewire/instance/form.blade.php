c6653b1322098d59d68e1c9f411b4e5046938a59
José Rafael <rafaelsilva9240@hotmail.com>
2024-03-15 05:57:19 -0300

    show instance form if auth user have one or more instance

--- resources/views/livewire/instance/form.blade.php
+++ resources/views/livewire/instance/form.blade.php
@@ -1,6 +1,6 @@
 <div class="">
     {{-- The whole world belongs to you. --}}
-    @if(count(Auth::user()->instances) === 0)
+    {{-- @if(count(Auth::user()->instances) === 0) --}}
     <x-form wire:submit="handleSubmit">
         <x-input label="Descrição" type="description" wire:model="description" placeholder="Minha instancia vivo" />
         <x-input label="Número" maxlength="13" hint="Número do telefone que se conectará" wire:model="phonenumber"
@@ -11,5 +11,5 @@
             <x-button label="Salvar Instancia" class="btn-primary" type="submit" spinner="handleSubmit" />
         </x-slot:actions>
     </x-form>
-    @endif
+    {{-- @endif --}}
 </div>
