3016b7ae616c36af663e05d7b07b97570a17775a
José Rafael <rafaelsilva9240@hotmail.com>
2024-03-22 03:09:03 -0300

    verify phonenumbers existence per batch

--- app/Service/Evolution/EvolutionChatService.php
+++ app/Service/Evolution/EvolutionChatService.php
@@ -17,12 +17,42 @@ class EvolutionChatService
         $this->webhookUrl = env("WEBHOOK_URL");
     }
 
+    public function divideArrayItems($items, $itemsPerBatch = 50)
+    {
+        $subArrays = [];
+        $count = 0;
+        $totalItems = count($items);
+
+        for ($i = 0; $i < $totalItems; $i += $itemsPerBatch) {
+            $subArrays[] = array_slice($items, $i, $itemsPerBatch);
+        }
+
+        // dd($subArrays);
+        return $subArrays;
+    }
+
+    public function makeManyRequests()
+    {
+        // se
+
+    }
+
     public function checkNumbersExistence($instanceName, $numbers = [])
     {
         if (!count($numbers)) {
             return false;
         }
 
+        $maxPhonenumbersPerRequest = 50;
+        if (count($numbers) >= $maxPhonenumbersPerRequest) {
+            $this->divideArrayItems($numbers, 50);
+        }
+
+        $numberBatches = $this->divideArrayItems($numbers);
+
+        $formattedNumbers = [];
+
+        // Faz uma requisição para cada lote de números
         $getNumbersRoute = '/chat/whatsappNumbers/' . $instanceName;
         $url = $this->apiUrl . $getNumbersRoute;
         $body = [
@@ -32,11 +62,36 @@ class EvolutionChatService
         $headers = [
             'apiKey' => $this->apiKey
         ];
+        foreach ($numberBatches as $batch) {
+            $body = ['numbers' => $batch];
 
-        $response = Http::withHeaders($headers)
-            ->post($url, $body);
-        $data = $response->json();
-        return $this->formatNumbers($data);
+            $response = Http::withHeaders($headers)->post($url, $body);
+            $data = $response->json();
+            $formatedPhonenumbers = $this->formatNumbers($data);
+            // dd($phonenumbersFormated);
+            // Formata os números retornados
+            $formattedNumbers += $this->formatNumbers($data);
+            // $formattedNumbers = array_merge($formattedNumbers, $formatedPhonenumbers);
+        }
+        // dd($formattedNumbers);
+
+        return $formattedNumbers;
+
+
+        // $getNumbersRoute = '/chat/whatsappNumbers/' . $instanceName;
+        // $url = $this->apiUrl . $getNumbersRoute;
+        // $body = [
+        //     'numbers' => $numbers,
+        // ];
+
+        // $headers = [
+        //     'apiKey' => $this->apiKey
+        // ];
+
+        // $response = Http::withHeaders($headers)
+        //     ->post($url, $body);
+        // $data = $response->json();
+        // return $this->formatNumbers($data);
     }
 
     private function formatNumbers($numbersInfo = [])
