38699b532798fc4ec8ac894b8b4c97d426799e7e
CartBuy <rafaelsilva9240@hotmail.com>
2024-04-25 04:48:27 -0300

    add logs to DonePhonenumberCheckJob

--- app/Jobs/DonePhonenumberCheckJob.php
+++ app/Jobs/DonePhonenumberCheckJob.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class DonePhonenumberCheckJob implements ShouldQueue
 {
@@ -28,14 +29,25 @@ class DonePhonenumberCheckJob implements ShouldQueue
      */
     public function handle(): void
     {
-        $countPhonenumbersToVerify = VerifiedPhonenumberCheck::query()->where([
-            'check_id' => $this->check->id
-        ])->count();
-        $countDoneVerifies = $this->check->verifies->where('verified', 1)->count();
-        if ($countPhonenumbersToVerify === $countDoneVerifies) {
-            $this->check->done = 1;
-            $this->check->save();
-            return;
+        try {
+            //code...
+            Log::info('init DonePhonenumberCheckJob');
+            $countPhonenumbersToVerify = VerifiedPhonenumberCheck::query()->where([
+                'check_id' => $this->check->id
+            ])->count();
+            $countDoneVerifies = $this->check->verifies->where('verified', 1)->count();
+            if ($countPhonenumbersToVerify === $countDoneVerifies) {
+                $this->check->done = 1;
+                $this->check->save();
+                return;
+            }
+            Log::info('end DonePhonenumberCheckJob', [
+                'countPhonenumbersToVerify' => $countPhonenumbersToVerify,
+                'countDoneVerifies' => $countDoneVerifies
+            ]);
+        } catch (\Exception $e) {
+            Log::error("error DonePhonenumberCheckJob", ['message' => $e->getMessage()]);
+            //throw $th;
         }
     }
 }
