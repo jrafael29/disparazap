61f18e3993f11186bd6ffa846e4153683bb39120
CartBuy <rafaelsilva9240@hotmail.com>
2024-05-01 13:51:49 -0300

    change horizon max processes

--- config/horizon.php
+++ config/horizon.php
@@ -202,7 +202,7 @@ return [
                 'queue' => ['default'],
                 'balance' => 'auto',
                 'minProcesses' => 1,
-                'maxProcesses' => 13,
+                'maxProcesses' => 10,
                 'balanceMaxShift' => 1,
                 'balanceCooldown' => 3,
                 'memory' => 128,
