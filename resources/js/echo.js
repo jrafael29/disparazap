7b8506beb6446f6405e0e7c24413aed6f619776e
CartBuy <rafaelsilva9240@hotmail.com>
2024-05-19 02:59:16 -0300

    remove wss from enabled transports on echo config

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -10,6 +10,6 @@ const echoConfig = {
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
+    enabledTransports: ['ws'],
 }
 window.Echo = new Echo(echoConfig);
